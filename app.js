8d6b64e81a91ab8fec854815db17f4b08af28821
Petya Petrova <92075297+PaskalisImmorral@users.noreply.github.com>
2022-06-16 12:17:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,81 @@
-function lockedProfile() {
-    Array.from(document.querySelectorAll('.profile button')).forEach(
-        b => b.addEventListener('click' , showMore));
+window.addEventListener('load', solve);
+
+function solve() {
+    let modelInput = document.getElementById('model');
+    let yearInput = document.getElementById('year');
+    let descriptionInput = document.getElementById('description');
+    let priceInput = document.getElementById('price');
+    let buttonAdd = document.getElementById('add');
+    let tBody = document.getElementById('furniture-list');
+    let totalPrice = document.querySelector('.total-price');
+    let priceOfBought = 0;
+    buttonAdd.addEventListener('click', addFunction);
+
+
+
+    function addFunction(e){
+        e.preventDefault();
+        let model = modelInput.value;
+        let price = Number(priceInput.value);
+        let year =  Number(yearInput.value);
+        let description = descriptionInput.value;
+        let trInfo = document.createElement('tr');
+        let tdModel = document.createElement('td');
+        let tdPrice = document.createElement('td');
+        let tdButtons = document.createElement('td');
+        let buttonMoreInfo = document.createElement('button');
+        let buttonBuy = document.createElement('button');
+        let trHide = document.createElement('tr');
+        let tdYear = document.createElement('td');
+        let tdDescription = document.createElement('td');
+
+        modelInput.textContent = '';
+        priceInput.textContent = '';
+        yearInput.textContent = '';
+        descriptionInput.textContent = '';
+
+        buttonMoreInfo.textContent = 'More Info';
+        buttonBuy.textContent = 'Buy it';
+        tdButtons.appendChild(buttonMoreInfo);
+        tdButtons.appendChild(buttonBuy);
+        trInfo.appendChild(tdModel);
+        trInfo.appendChild(tdPrice);
+        trInfo.appendChild(tdButtons);
+        tBody.appendChild(trInfo);
+        tdModel.textContent = model;
+        tdPrice.textContent = price.toFixed(2);
+        
+        buttonMoreInfo.classList.add('moreBtn');
+        
+        buttonBuy.classList.add('buyBtn');
+        
+        
+        buttonMoreInfo.addEventListener('click', showMore);
+        buttonBuy.addEventListener('click', buyFunction);
+        function buyFunction(e){
+            priceOfBought += Number(tdPrice.textContent); 
+            totalPrice.textContent = priceOfBought.toFixed(2);
+            trInfo.remove();
+            trHide.remove();
+        }
+    
     function showMore(e){
-        let profile = e.target.parentElement;    
-        let isActive = profile.querySelector
-    ('input[type="radio"][value="unlock"]').checked;
-        if (isActive){
-        let div = profile.querySelector('div');
-        if (e.target.textContent == 'Show more'){
-         e.target.textContent = 'Hide it'
-        div.style.display = 'block';
-        } else {
-           e.target.textContent = 'Show more';
-           div.style.display = 'none';
-       }
-   }
+        
+        
+        trHide.classList.add('hide');
+        trHide.style.display = 'contents';
+        trHide.appendChild(tdYear);
+        trHide.appendChild(tdDescription);
+        tBody.appendChild(trHide);
+        tdYear.textContent = 'Year: ' + year;
+        tdDescription.textContent = 'Description: ' + description;
+        tdDescription.setAttribute('colspan',3);
+        if(buttonMoreInfo.textContent == 'More Info'){
+            buttonMoreInfo.textContent = 'Less Info';
+        } else if (buttonMoreInfo.textContent == 'Less Info'){
+            buttonMoreInfo.textContent = 'More Info';
+            trHide.style.display = 'none';
+        }
+    }
 }
 }
